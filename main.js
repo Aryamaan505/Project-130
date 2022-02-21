bdb8c2a7d755d0f45b85b1c537dd87b1c786ae05
Aryamaan505 <83063969+Aryamaan505@users.noreply.github.com>
2022-02-21 19:40:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+song = "";
+
+function preload()
+{
+	song = loadSound("music.mp3");
+}
+
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function draw() {
+	image(video, 0, 0, 600, 500);
+
+	fill("#FA8072");
+	stroke("#FA8072");
+if (right_wrist_score>0.2) {
+	circle(right_wrist_x,right_wrist_y,20);
+
+if (right_wrist_y>0&& right_wrist_y<= 100) {
+	document.getElementById("speed").innerHTML="Speed=0.5 x";
+	song.rate(0.5);
+} else if(right_wrist_y>100&& right_wrist_y<= 200){
+	document.getElementById("speed").innerHTML="Speed=1 x";
+	song.rate(1);	
+}else if(right_wrist_y>200&& right_wrist_y<= 300){
+	document.getElementById("speed").innerHTML="Speed=1.5  x";
+	song.rate(1.5);
+}else if(right_wrist_y>300&& right_wrist_y<= 400){
+	document.getElementById("speed").innerHTML="Speed=2  x";
+	song.rate(2);
+}else if(right_wrist_y>400&& right_wrist_y<= 500){
+	document.getElementById("speed").innerHTML="Speed=2.5  x";
+	song.rate(2.5);
+}
+	
+}	
+
+	if (score_left_wrist > 0.2) {
+		circle(left_wrist_x,left_wrist_y,20);
+		InNumberleftWristY=Number(left_wrist_y);
+		removeDecimals=floor(InNumberleftWristY);
+		left_wrist_y_divide_1000=removeDecimals/1000;
+		volume=left_wrist_y_divide_1000*2;
+		document.getElementById("volume").innerHTML="Volume="+ volume ;
+		song.setVolume(volume);		
+	}
+}
+
+function  modelLoaded() {
+	console.log('Mostly no errors, PoseNet is initialized so be happy about the first step');
+}
+
+
+function  play() {
+    song.play();
+	song.setVolume(1);
+	song.rate(1);
+
+}
+left_wrist_x=0;
+left_wrist_y=0;
+right_wrist_x=0;
+right_wrist_y=0;
+
+function  gotPoses(results) {
+	if (results.length>0) {
+		console.log(results);
+		right_wrist_score=results[0].pose.keypoints[10].score;
+		score_left_wrist=results[0].pose.keypoints[9].score;
+		console.log("score_left_wrist="+score_left_wrist +"right_wrist_score="+right_wrist_score);
+		left_wrist_x=results[0].pose.leftWrist.x;
+		left_wrist_y=results[0].pose.leftWrist.y;
+		console.log("left wrist x = "+ left_wrist_x+" left wrist y= " + left_wrist_y);
+		right_wrist_x=results[0].pose.rightWrist.x;
+		right_wrist_y=results[0].pose.rightWrist.y;
+		console.log("right wrist x = "+ right_wrist_x+" right wrist y= " + right_wrist_y);
+	}
+}
